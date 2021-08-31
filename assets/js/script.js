f5f88bc4e9fed63fce693a39f4347c5fcfd095f4
Karine Vieira <kv.vieira1@gmail.com>
2021-08-31 08:16:17 -0300

    Add slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,38 @@ menu.addEventListener('click', () => {
         ul.style.display = 'flex'
     }
 })
+
+let totalSlides = document.querySelectorAll('.slider_item').length
+let currentSlide = 0
+
+let sliderWidth = document.querySelector('.slider').clientWidth
+
+// document.querySelector('.slider_width').style.width = `calc(100vw * ${totalSlides})`
+document.querySelector('.slider_width').style.width = `${sliderWidth * totalSlides}px`
+
+document.querySelector('.slider_controls').style.width = `${sliderWidth}px`
+document.querySelector('.slider_controls').style.height = `${document.querySelector('.slider').clientHeight}px`
+
+function goPrev(){
+    currentSlide--;
+    if(currentSlide < 0){
+        currentSlide = totalSlides - 1
+    }
+    updateMargin()
+}
+
+function goNext(){
+    currentSlide++
+    if(currentSlide > (totalSlides - 1)){
+        currentSlide = 0
+    }
+    updateMargin()
+}
+
+function updateMargin(){
+    let sliderItemWidth = document.querySelector('.slider_item').clientWidth
+    let newMargin = (currentSlide * sliderItemWidth)
+    document.querySelector('.slider_width').style.marginLeft = `-${newMargin}px`
+}
+
+setInterval(goNext, 5000)
